70dbf5e198daa4180448b38e3d57015aff48784f
ysk <499428620@qq.com>
2023-06-11 08:21:32 +0800

    初始化代码

---
+++ src/main/java/com/ysk/MyStudyCodeApplication.java
@@ -0,0 +1,13 @@
+package com.ysk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyStudyCodeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyStudyCodeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ysk/MyStudyCodeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ysk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyStudyCodeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
