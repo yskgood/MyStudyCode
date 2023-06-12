b6110cae2c6cebf56f87624655ace40b8f55fca2
ysk <499428620@qq.com>
2023-06-13 07:37:54 +0800

    454. 四数相加 II

---
+++ src/main/java/com/ysk/leetcode/hash/FourSumCount.java
@@ -0,0 +1,11 @@
+package com.ysk.leetcode.hash;
+
+/**
+ * 454. 四数相加 II
+ */
+public class FourSumCount {
+
+    public int fourSumCount(int[] nums1, int[] nums2, int[] nums3, int[] nums4) {
+        return 0;
+    }
+}
