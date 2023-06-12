e9c42db024b4101dedfab26981ada20f4c5d2462
ysk <499428620@qq.com>
2023-06-13 07:36:37 +0800

    18. 四数之和

---
+++ src/main/java/com/ysk/leetcode/hash/FourSum.java
@@ -0,0 +1,13 @@
+package com.ysk.leetcode.hash;
+
+import java.util.List;
+
+/**
+ * 18. 四数之和
+ */
+public class FourSum {
+
+    public List<List<Integer>> fourSum(int[] nums, int target) {
+        return null;
+    }
+}
