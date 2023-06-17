78266dd1b166bd9376936145d8152f2a0958802e
shaokai.ysk <shaokai.ysk@alibaba-inc.com>
2023-06-17 20:34:22 +0800

    17：电话好吗的字母组合

--- src/main/java/com/ysk/leetcode/recall/LetterCombinations.java
+++ src/main/java/com/ysk/leetcode/recall/LetterCombinations.java
@@ -6,38 +6,54 @@ import java.util.*;
  * 17：电话好吗的字母组合
  * 思路：
  * 三个要素：选择 (Options)，限制 (Restraints)，结束条件 (Termination)
- * 1、结束条件：字符长度 3^n次方
- * 2、选择：每个数字选择三次
- * 3、限制：
  *
  * @author ysk
  * @date 2023/6/16 5:14 PM
  */
 public class LetterCombinations {
-    public List<String> letterCombinations(String digits) {
+    public static List<String> letterCombinations(String digits) {
         List<String> ans = new LinkedList<>();
         char[] digitStrArray = digits.toCharArray();
         //字母和对应数字的映射
-        Map<Character, Integer> dcMap = new HashMap<>();
+        Map<Integer, List<String>> digitCharMap = new HashMap<>();
+        char c = 'a';
+        for (int i = 2; i <= 9; i++) {
+            digitCharMap.put(i, new ArrayList<>());
+            for (int j = 0; j < (i == 9 || i == 7 ? 4 : 3); j++) {
+                digitCharMap.get(i).add(String.valueOf(c++));
+            }
+
+        }
+        //转换成数字数组
+        int[] digitArray = new int[digitStrArray.length];
+        if (digitArray.length == 0) {
+            return ans;
+        }
         for (int i = 0; i < digitStrArray.length; i++) {
             int digit = Character.getNumericValue(digitStrArray[i]);
-            //每个字母映射三个字母
-            for (int j = 0; j < 3; j++) {
-
-            }
+            digitArray[i] = digit;
         }
+        backTracing("", 0, digitArray, digitCharMap, ans);
         return ans;
     }
 
-    private void backTracing(String str, Map<Character, Integer> dcMap, List<String> ans, int len) {
+    private static void backTracing(String str, int digIndex, int[] digitArray, Map<Integer, List<String>> digitCharMap, List<String> ans) {
         //结束条件
-        if (str.length() == len) {
+        if (str.length() == digitArray.length) {
             ans.add(str);
             return;
         }
-        //选择：
-
-        //限制：相同数字的字母只能出现一次
-
+        //取处理的数字
+        int digit = digitArray[digIndex];
+        //取数字对应的Map
+        List<String> charList = digitCharMap.get(digit);
+        //循环处理Map的字符
+        for (int i = 0; i < charList.size(); i++) {
+            //递归继续深度遍历
+            //注意：str和digIndex不能直接修改，影响回溯后的值
+            backTracing(str + charList.get(i), digIndex + 1, digitArray, digitCharMap, ans);
+        }
     }
+
+
 }
---
+++ src/test/java/com/ysk/leetcode/recall/LetterCombinationsTest.java
@@ -0,0 +1,19 @@
+package com.ysk.leetcode.recall;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * @author ysk
+ * @date 2023/6/17 10:08 AM
+ */
+public class LetterCombinationsTest {
+
+    @Test
+    void letterCombinations() {
+        System.out.println(LetterCombinations.letterCombinations("2"));
+        System.out.println(LetterCombinations.letterCombinations("23"));
+        System.out.println(LetterCombinations.letterCombinations("237"));
+    }
+}
