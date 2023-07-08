1852edc51a3cc84d1fe1371261fdd638b435a94c
ysk <499428620@qq.com>
2023-07-09 07:48:58 +0800

    199. 二叉树的右视图

---
+++ src/main/java/com/ysk/tree/binary/RightSideView.java
@@ -0,0 +1,45 @@
+package com.ysk.tree.binary;
+
+import com.ysk.tree.TreeNode;
+
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+/**
+ * 199. 二叉树的右视图
+ * 没层中最靠右边的节点
+ */
+public class RightSideView {
+
+    public List<Integer> rightSideView(TreeNode root) {
+        List<Integer> ans = new LinkedList<>();
+        if (root == null) {
+            return ans;
+        }
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            //取出本层所有节点，并放入下层到队列中
+            for (int i = 0; i < size; i++) {
+                TreeNode node = queue.poll();
+                //最后一个节点放入结果
+                if (i == (size - 1)) {
+                    ans.add(node.val);
+                }
+                //放入下层节点
+                if (node.left != null) {
+                    queue.offer(node.left);
+                }
+                if (node.right != null) {
+                    queue.offer(node.right);
+                }
+            }
+
+        }
+
+        return ans;
+    }
+}
