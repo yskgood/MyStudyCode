05663316156f6a960ab5d3703a4594f0fea05ea5
ysk <499428620@qq.com>
2023-07-08 15:12:37 +0800

    144. 二叉树的前序遍历--遍历实现

--- src/main/java/com/ysk/tree/binary/PreorderTraversal.java
+++ src/main/java/com/ysk/tree/binary/PreorderTraversal.java
@@ -4,6 +4,7 @@ import com.ysk.tree.TreeNode;
 
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Stack;
 
 /**
  * 144. 二叉树的前序遍历
@@ -13,7 +14,7 @@ public class PreorderTraversal {
 
     public List<Integer> preorderTraversal(TreeNode root) {
         List<Integer> ans = new LinkedList<>();
-        preorder(root,ans);
+        preorder(root, ans);
         return ans;
     }
 
@@ -28,5 +29,33 @@ public class PreorderTraversal {
         preorder(cur.left, ans);
         //右
         preorder(cur.right, ans);
+    }
+
+
+    /**
+     * 采用栈遍历的方式实现
+     *
+     * @param root
+     * @return
+     */
+    public List<Integer> preorderTraversal2(TreeNode root) {
+        List<Integer> ans = new LinkedList<>();
+        if(root==null){
+            return ans;
+        }
+        Stack<TreeNode> treeNodeStack = new Stack<>();
+        treeNodeStack.push(root);
+        while (!treeNodeStack.isEmpty()) {
+            TreeNode parent = treeNodeStack.pop();
+            ans.add(parent.val);
+            //先处理左树，所以要先让右边进去
+            if (parent.right != null) {
+                treeNodeStack.push(parent.right);
+            }
+            if (parent.left != null) {
+                treeNodeStack.push(parent.left);
+            }
+        }
+        return ans;
     }
 }
