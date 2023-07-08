23c6c8f642b33945ac27b3da933266f20909f3d6
ysk <499428620@qq.com>
2023-07-09 07:29:26 +0800

    107. 二叉树的层序遍历 II

---
+++ src/main/java/com/ysk/tree/binary/LevelOrderBottom.java
@@ -0,0 +1,50 @@
+package com.ysk.tree.binary;
+
+import com.ysk.tree.TreeNode;
+
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+/**
+ * 107. 二叉树的层序遍历 II
+ * 返回其节点值 自底向上的层序遍历
+ * <p>
+ * 思路：正常遍历，在将结果反转
+ *
+ * @see LevelOrder
+ */
+public class LevelOrderBottom {
+
+    public List<List<Integer>> levelOrderBottom(TreeNode root) {
+        List<List<Integer>> ans = new LinkedList<>();
+        if (root == null) {
+            return ans;
+        }
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+
+        while (!queue.isEmpty()) {
+            List<Integer> ls = new LinkedList<>();
+            //当前行的节点数量
+            int size = queue.size();
+            for (int i = 0; i < size; i++) {
+                TreeNode node = queue.poll();
+                ls.add(node.val);
+                if (node.left != null) {
+                    queue.offer(node.left);
+                }
+                if (node.right != null) {
+                    queue.offer(node.right);
+                }
+            }
+            ans.add(ls);
+        }
+
+        //反转
+        Collections.reverse(ans);
+
+        return ans;
+    }
+}
