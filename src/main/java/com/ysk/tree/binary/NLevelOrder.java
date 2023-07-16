dc946e92e8289b871caa3d3face572dd5f786378
ysk <499428620@qq.com>
2023-07-17 06:40:51 +0800

    101. 对称二叉树

---
+++ src/main/java/com/ysk/tree/binary/NLevelOrder.java
@@ -0,0 +1,37 @@
+package com.ysk.tree.binary;
+
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+/**
+ * 429. N 叉树的层序遍历
+ */
+public class NLevelOrder {
+
+    public List<List<Integer>> levelOrder(Node root) {
+
+        List<List<Integer>> ans = new LinkedList<>();
+        if (root == null) {
+            return ans;
+        }
+
+        Queue<Node> queue = new LinkedList<>();
+        queue.offer(root);
+        while (!queue.isEmpty()) {
+            List<Integer> ls = new LinkedList<>();
+            int size = queue.size();
+            for (int i = 0; i < size; i++) {
+                Node node = queue.poll();
+                ls.add(node.val);
+                //添加所有子节点
+                for (Node child : node.children) {
+                    queue.offer(child);
+                }
+            }
+            ans.add(ls);
+        }
+
+        return ans;
+    }
+}
