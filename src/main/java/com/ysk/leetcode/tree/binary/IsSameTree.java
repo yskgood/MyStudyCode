446c6ac5cc710f90a92489c0c628504be8f24eb7
ysk <499428620@qq.com>
2023-07-29 06:31:16 +0800

    100. 相同的树

--- src/main/java/com/ysk/leetcode/tree/binary/IsSameTree.java
+++ src/main/java/com/ysk/leetcode/tree/binary/IsSameTree.java
@@ -4,10 +4,33 @@ import com.ysk.leetcode.tree.TreeNode;
 
 /**
  * 100. 相同的树
+ * 思路：用递归的方式，看每个树的相同节点是否一致
+ *
+ * @see IsSymmetric
  */
 public class IsSameTree {
 
     public boolean isSameTree(TreeNode p, TreeNode q) {
-        return true;
+        return sameTreeNode(p, q);
+    }
+
+    private boolean sameTreeNode(TreeNode p, TreeNode q) {
+        //为空情况判断
+        if (p == null && q == null) {
+            return true;
+        } else if (p != null && q == null) {
+            return false;
+        } else if (p == null && q != null) {
+            return false;
+        }
+        //有值情况判断
+        if (p.val != q.val) {
+            return false;
+        }
+
+        boolean isLeftSame = sameTreeNode(p.left, q.left);
+        boolean isRightSame = sameTreeNode(p.right, q.right);
+        return isLeftSame && isRightSame;
+
     }
 }
--- src/main/java/com/ysk/leetcode/tree/binary/IsSubtree.java
+++ src/main/java/com/ysk/leetcode/tree/binary/IsSubtree.java
@@ -2,12 +2,63 @@ package com.ysk.leetcode.tree.binary;
 
 import com.ysk.leetcode.tree.TreeNode;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 /**
  * 572. 另一棵树的子树
+ * 思路：依次看root的每个节点子树是否和subRoot一样，如果有一样的退出
+ *
+ * @see IsSameTree
  */
 public class IsSubtree {
 
     public boolean isSubtree(TreeNode root, TreeNode subRoot) {
-        return true;
+        if (root == null && subRoot == null) {
+            return true;
+        }
+        if (root == null && subRoot != null) {
+            return false;
+        }
+        if (root != null && subRoot == null) {
+            return false;
+        }
+        //遍历root每个节点
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+        while (!queue.isEmpty()) {
+            TreeNode currentNode = queue.poll();
+            if (isSameTree(currentNode, subRoot)) {
+                return true;
+            }
+            //加入下一层
+            if (currentNode.left != null) {
+                queue.offer(currentNode.left);
+            }
+            if (currentNode.right != null) {
+                queue.offer(currentNode.right);
+            }
+        }
+
+        return false;
+    }
+
+    private boolean isSameTree(TreeNode p, TreeNode q) {
+        //为空情况判断
+        if (p == null && q == null) {
+            return true;
+        } else if (p != null && q == null) {
+            return false;
+        } else if (p == null && q != null) {
+            return false;
+        }
+        //有值情况判断
+        if (p.val != q.val) {
+            return false;
+        }
+
+        boolean isLeftSame = isSameTree(p.left, q.left);
+        boolean isRightSame = isSameTree(p.right, q.right);
+        return isLeftSame && isRightSame;
     }
 }
